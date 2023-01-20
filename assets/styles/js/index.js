d4ec1f0ca97e0a19470354acf05bf3666870a94c
Syndicalix <speich.sebastien@gmail.com>
2023-01-20 09:59:31 +0000

    base de données JS

---
+++ assets/styles/js/index.js
@@ -0,0 +1,6 @@
+import { User } from '.assets/styles/js/classes/User.js';  
+import { Book } from ".assets/styles/js/classes/Book.js";  
+  
+window.addEventListener("DOMContentLoaded", function(){  
+    createApp(App).mount('#app');  
+});
